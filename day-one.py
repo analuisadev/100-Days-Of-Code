8bb2f2648edc7bcfbf1155da14cb2f0248f6e03a
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2020-11-28 11:19:58 -0300

    Update day-one.py

--- day-one.py
+++ day-one.py
@@ -1,3 +1,4 @@
+from time import sleep
 print ('\033[33m -=-\033[m' *7)
 #unity1 is having a problem with the terminal spacing, just enter it back to the place
 unity1 = float(input('\033[1;37mYour note from first unity: \033[m'))
@@ -8,9 +9,8 @@ average = (unity1 + unity2 + unity3 + unity4) /4
 print ('\033[33m -=-\033[m' * 7)
 print ('\033[1;37mCalculating your average...\033[m')
 print ('\033[33m -=-\033[m' * 7)
-sleep(1)
+sleep (1)
 if average >= 5:
-
     print ('\033[1;33m      AMAZING! Your PASSED!\033[m')
 else:
     print ('\033[1;31m      WHAT A PITY! Your not PASSED!\033[m')
