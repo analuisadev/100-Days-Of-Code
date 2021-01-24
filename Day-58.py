099d97c2fc0b54eec7be90098709e7e58e456cc9
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-01-24 08:42:21 -0300

    Day-58.py

---
+++ Day-58.py
@@ -0,0 +1,9 @@
+def write(msg):
+    size = len(msg)
+    print('=' * size)
+    print (msg)
+    print ('=' * size)
+    
+    
+write  ("Hello World, I'm here")
+write  ("My name is Ana Luisa and I'm learning functions in python")
