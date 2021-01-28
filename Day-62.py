12fe4074842e991aca20c003c62d708b75eb6473
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-01-28 13:24:39 -0300

    Day-62.py

---
+++ Day-62.py
@@ -0,0 +1,19 @@
+#importação
+from Viagem import Viagem
+viagens_opcoes = ["", "1) Bahia", "2) São Paulo", "3) Estados Unidos", "4) Canadá", "5) Portugal", "6) África do Sul", "7) Rússia" ]
+#inicio do programa
+print ('{:=^40}'.format(' CADASTRO DE VIAGEM '))
+nome = input('Digite o seu nome: ')
+print ('{:=^40}'.format(' OPÇÕES DE DESTINO '))
+opcao = int(input('''1) Bahia
+2) São Paulo
+3) Estados Unidos
+4) Portugal 
+5) Canadá
+6) África do Sul
+7) Rússia 
+Selecione a colocação correspondente a sua viagem: '''))
+if opcao > 7: 
+    print ('Sua opção de viagem está invalida, por favor digite o número correspondente ao destino corretamente...')
+else:
+    print (f'Você escolheu o {opcao}° destino, tenha uma ótima viagem :)')
