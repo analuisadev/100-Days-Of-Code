30182605f0266ed37e621cf96f64dc10fbe766ba
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-01-28 13:27:45 -0300

    Update Day-62.py

--- Day-62.py
+++ Day-62.py
@@ -1,6 +1,5 @@
 #importação
 from Viagem import Viagem
-viagens_opcoes = ["", "1) Bahia", "2) São Paulo", "3) Estados Unidos", "4) Canadá", "5) Portugal", "6) África do Sul", "7) Rússia" ]
 #inicio do programa
 print ('{:=^40}'.format(' CADASTRO DE VIAGEM '))
 nome = input('Digite o seu nome: ')
