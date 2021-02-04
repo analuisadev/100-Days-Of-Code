f85c608ddd3cca4960a6f3df72ba521b8ea57843
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-02-04 11:34:03 -0300

    Exercitando Módulos em Python

---
+++ Day-68.py/Day-68.py
@@ -0,0 +1,17 @@
+from Moeda import diminuir, aumentar, dobro, metade
+from time import sleep
+"""
+    Programa para exercitar Módulos em Python
+    Objetivo do programa: 
+    - Receber um valor inteiro informado pelo usuário
+    - Mostrar esse valor com: 
+        Aumento de 10%
+        Dobro do valor 
+        Métade do valor 
+"""
+num  = float(input('Digite um valor: R$ '))
+print ('PROCESSANDO...')
+sleep(1)
+print (f"O valor R${num} com 10% de aumento é {aumentar(num, 10)}")
+print (f'O dobro do valor R${num} é {dobro(num)}')
+print (f'A Métade do valor R${num} é {metade(num)}')
---
+++ Day-68.py/Moeda.py
@@ -0,0 +1,13 @@
+def aumentar (valor, taxa): 
+    total = valor + (valor * taxa/100)
+    return total
+def diminuir (valor, taxa): 
+    total = valor - (valor * taxa/100)
+    return total
+def dobro(valor):
+    total = valor  * 2
+    return total 
+def metade (valor):
+    total = valor / 2
+    return total 
+
