c8ce14c415cfc6ff7ef5ab1f91061b18417ec5b4
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-02-15 16:07:05 -0300

    atualização

--- Day_74_75_76/sistema.py
+++ Day_74_75_76/sistema.py
@@ -1,23 +1,31 @@
+#Importação de todos os arquivos referentes as pastas criadas dentro do exercício
 from Day_74_75_76.lib.interface import *
 from Day_74_75_76.lib.arquivo import *
+#Importação do sleep para dar um descanso
 from time import sleep
 
-arquivo = 'ExercicioCursoEmVideo.txt'
-if not arquivoExiste(arquivo):
-    criarArquivo(arquivo)
-else:
-    print ('O Arquivo não foi encontrado')
-    criarArquivo
+arq = 'exerciciodocursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+#Looping infinito para o sistema principal
 while True:
-    resp = menu(['Ver pessoas cadastradas', 'Cadastrar novas pessoas', 'Sair do sistema'])
-    if resp == 1:
-        #opção para listar o conteúdo de um arquivo
-        lerArquivo(arquivo)
-    elif resp == 2:
-        cabeçalho('Opção 2')
-    elif resp == 3:
-        cabeçalho('Saindo do sistema... \nVolte sempre! :)')
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar uma nova pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        #Opção para listar o conteúdo de um arquivo
+        lerArquivo(arq)
+    elif resposta == 2:
+        #Opção para cadastrar uma nova pessoa
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome:'))
+        idade = leiaInt(('Idade: '))
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        #Opção para sair do sistema
+        cabeçalho('Encerrando o sistema, Volte sempre!')
         break
     else:
-        print('\033[1;31mATENÇÃO: Digite uma opção válida!\033[m')
+        #Digitou uma opção indisponível no menu
+        print ('\033[1;31mERRO: Você digitou uma opção indisponível no menu!\033[m')
         sleep(2)
