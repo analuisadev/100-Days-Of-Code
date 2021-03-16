f58836243f07e02ef6d2c2fb52f75d35718e4d53
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-03-16 12:43:38 -0300

    Abrindo uma janela usando Pygame
    Primeiros passos usando o Pygame

---
+++ Day-95.py
@@ -0,0 +1,23 @@
+#Importações necessárias para a criação da janela
+import pygame
+from pygame.locals import *
+from sys import exit
+
+#Inicialização das váriaveis e funções do pygame
+pygame.init()
+
+#Criação da tela
+width = 640
+height = 480
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Game')
+
+#Looping principal do jogo
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+
+
