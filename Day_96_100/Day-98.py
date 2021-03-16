55114719b7c6af14a397b2cf1223f1a763f030d4
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-03-16 14:24:15 -0300

    Controlando objetos

---
+++ Day_96_100/Day-98.py
@@ -0,0 +1,55 @@
+""""
+Parte 4: Controlando objetos
+"""
+#Importações necessárias para a criação da janela
+import pygame
+from pygame.locals import *
+from sys import exit
+
+#Inicialização das váriaveis e funções do pygame
+pygame.init()
+
+#Criação da tela
+width = 640
+height = 480
+x = width/2
+y = height/2
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Game')
+#Controlando a velocidade da movimentação do objeto
+clock = pygame.time.Clock()
+
+#Looping principal do jogo
+while True:
+    clock.tick(40)
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+#Criando uma condição para mudar a movimentação de acordo com a tecla apertada
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+               x = x - 20
+            if event.key == K_d:
+                x = x + 20
+            if event.key == K_w:
+                y = y - 20
+            if event.key == K_s:
+                y = y + 20
+    # Criando uma condição caso a tecla continue a ser pressionada
+    if pygame.key.get_pressed()[K_a]:
+        x = x - 20
+    if pygame.key.get_pressed()[K_d]:
+        x = x + 20
+    if pygame.key.get_pressed()[K_w]:
+        y = y - 20
+    if pygame.key.get_pressed()[K_s]:
+        y = y + 20
+
+#Desenhando Objetos dentro da Tela e movimentando
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, 40, 50))#
+
+    pygame.display.update()
+
+
