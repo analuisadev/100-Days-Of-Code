8e441a4366128cf32844900b0fd322e0c5c58c1e
Ana Luisa Santos <71856519+analuisadev@users.noreply.github.com>
2021-03-16 12:43:00 -0300

    .

--- Day_95_96_97_98/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask #importação do flask
-
-app = Flask(__name__) #instancia do flask
-
-from app.controllers import default
--- Day_95_96_97_98/app/controllers/default.py
+++
@@ -1,6 +0,0 @@
-from Day_95_96_97_98 import app
-
-
-@app.route("/") #definindo rota para a página
-def index(): #função para pag principal
-    return "Hello World"
